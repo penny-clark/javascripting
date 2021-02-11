ee8b5a3d519b0a5f741cac91115c02c01fcd8bbc
Penny Clark <respect.cactus@gmail.com>
2021-02-11 16:47:54 +0000

    Initial commit of my javascripting work

---
+++ looping-through-arrays.js
@@ -0,0 +1,13 @@
+let pets = ['cat', 'dog', 'rat'];
+for (i =0; i < pets.length; i++) {
+pets[i] = pets[i] + 's'}
+console.log(pets);
+
+//javascripting solution
+const pets = ['cat', 'dog', 'rat']
+    
+for (let i = 0; i < pets.length; i++) {
+  pets[i] = pets[i] + 's'
+}
+
+console.log(pets)
